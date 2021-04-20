e55634bdd0226065ca86fc2ecb8c321adc5b9c18
서예리 <codeyeri7@gmail.com>
2021-04-21 02:13:27 +0900

    0420 homework

--- 2021.04/0420/SWEA_1865_동철이의일분배_서예리.py
+++ 2021.04/0420/SWEA_1865_동철이의일분배_서예리.py
@@ -1,3 +1,36 @@
+def dfs(idx):
+    global max_ans, ans
+    if idx == N:  # idx와 N이 같다면
+        if max_ans < ans:  # ans가 max_ans보다 클 때(최대값을 구해야하니)
+            max_ans = ans  # 최종 값을 max_ans로 갱신!
+            return
+    if max_ans > ans:  # ans가 최대값보다 작으면 다시 돌아가
+        return
+    if idx < N:
+        for i in range(N):
+            if visit[i] == 0:  # 아직 방문하지 않은 곳만
+                # zero division 에러가 떴다.
+                # 확률이 0인 것을 곱해주면 ans가 0이 되는데
+                # 이걸 밑에서 arr[idx][i]로 나누려니까 0을 나눌 수 없다며 에러가 뜸.
+                # 그래서 arr[idx][i]가 0이 아닐때만 돌도록 바꿔줌.
+                if arr[idx][i] != 0:
+                    visit[i] = 1  # 방문체크
+                    ans *= arr[idx][i]  # 확률값 추가
+                    dfs(idx+1)  # 다음 줄 돌기!
+                    visit[i] = 0  # 돌고 왔으면 다시 원상복구
+                    ans /= arr[idx][i]  # 값도 다시 빼주기
+
+
 for T in range(1, int(input())+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+    visit = [0] * N  # 방문체크
+    ans = 1  # 확률
+    max_ans = -1  # 최대값을 구하기 위한 값
+    for i in range(N):
+        for j in range(N):
+            arr[i][j] = round(arr[i][j] * 0.01, 2)
+    dfs(0)  # dfs로 돌기!
+    print('#{}'.format(T), end=' ')
+    # 소수점 6째자리까지 나오기 위
+    print('{:.6f}'.format(float(max_ans*100)))
--- 2021.04/0420/SWEA_5209_최소생산비용_서예리.py
+++ 2021.04/0420/SWEA_5209_최소생산비용_서예리.py
@@ -1,7 +1,29 @@
+def dfs(idx):
+    global min_ans, ans
+    # N번째 공장까지 다 가봤으면 최종 비용 저장
+    if idx == N:
+        # idx가 N이고, ans가 최소비용보다 작을 때에만!
+        # 이걸 추가 안 했더니 값이 다르게 나왔음.
+        if min_ans > ans:
+            min_ans = ans
+    # 지금까지 나온 최소비용보다 크다면 바로 돌려보내기
+    if min_ans <= ans:
+        return
+    # 공장돌면서 비용 비교하기
+    for i in range(N):
+        if visit[i] == 0:  # 아직 방문 안 한 곳만 가기
+            visit[i] = 1  # 방문체크
+            ans += arr[idx][i]  # 비용 추가
+            dfs(idx+1)  # 다음 줄로 이동
+            visit[i] = 0  # 갔다왔으면 원상복구
+            ans -= arr[idx][i]  # 추가했던 비용도 다시 빼기
+
+
 for T in range(1, int(input())+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+    visit = [0] * N  # 방문체크용
     ans = 0
-    visit = [0] * N
-    min_ans = 99 * N
-
+    min_ans = 987654321  # 최소비용 비교를 위한 값
+    dfs(0)
+    print("#{} {}".format(T, min_ans))
